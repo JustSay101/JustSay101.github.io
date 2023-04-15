9079eef1c7ecf782acc1a3af9d6d7e75abc04e78
LBG-JuhoT <juho.tommolalaune@gmail.com>
2023-04-15 10:57:07 +0300

    Initial commit of the website

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const THREE = window.MINDAR.IMAGE.THREE;
+const mindARThree = new window.MINDAR.IMAGE.MindARThree({
+    container: document.body,
+    imageTargetSrc: "../assets/targets/targets.mind"
+});
+
+const { renderer, scene, camera } = mindARThree;
+
+init()
+StartUpdate()
+
+async function init() 
+{
+    const container = document.createElement("div")
+    document.body.appendChild(container)
+    scene.name = "ARBusinessCard"
+    scene.add(camera)
+
+    console.log(camera);
+
+    await mindARThree.start();
+}
+
+function StartUpdate() 
+{
+    renderer.setAnimationLoop(OnUpdate)
+}
+
+function OnUpdate(timeStamp, frame) 
+{
+    renderer.render(scene, camera)
+}
