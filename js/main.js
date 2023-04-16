8352abe63577904d8c68ca94c6df8105443a043d
LBG-JuhoT <juho.tommolalaune@gmail.com>
2023-04-16 09:22:27 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,54 +1,40 @@
-import * as THREE from "../libs/three.js-r132/build/three.module.js"
-//const THREE = window.MINDAR.IMAGE.THREE;
+//import * as THREE from "../libs/three.js-r132/build/three.module.js"
+//import '../node_modules/mind-ar/dist/mindar-image-aframe.prod.js'
+
+import * as THREE from 'three';
+import { MindARThree } from 'mindar-image-three';
+
+const container = document.createElement("div")
+    document.body.appendChild(container)
+
 const mindARThree = new window.MINDAR.IMAGE.MindARThree({
-    container: document.body,
+    container: container,
     imageTargetSrc: "../assets/targets/targets.mind"
 });
 
-//const { renderer, scene, camera } = mindARThree;
+const { renderer, scene, camera } = mindARThree;
 
-let camera, renderer, scene
-const container = document.createElement("div")
-container.id = "container"
+//let camera, renderer, scene
 
 init()
 StartUpdate()
 
 async function init() 
 {
-    const container = document.createElement("div")
-    document.body.appendChild(container)
+    document.body.appendChild(renderer.domElement)
 
-    // Create a scene
-    scene = new THREE.Scene()
-    scene.name = "myScene"
-    
-    // Create a camera
-    camera = new THREE.PerspectiveCamera
-    (
-        60, 
-        window.innerWidth / window.innerHeight, 
-        0.1, 
-        1000
-    )
-    
-    camera.position.set(0, 0, 5)
-    
-    // Create a renderer
-    renderer = new THREE.WebGLRenderer
-    ({
-        antialias: true,
-        alpha: true
-    })
     renderer.setPixelRatio(window.devicePixelRatio)
     renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.xr.enabled = true
-    container.appendChild(renderer.domElement)
-    console.log(renderer)
 
-    await mindARThree.start();
+    // Create a scene
+    /*
+    scene = new THREE.Scene()
+    scene.name = "myScene"
+    */
 
-    container.appendChild(document.getElement("video"))
+    scene.add(camera)
+
+    await mindARThree.start()
 }
 
 function StartUpdate() 
