0c1909f0d43a1584587a91843d2957c8c3ae66e7
LBG-JuhoT <juho.tommolalaune@gmail.com>
2023-04-17 08:49:00 +0300

    Retry

--- js/main.js
+++ js/main.js
@@ -4,53 +4,22 @@
 import * as THREE from 'three';
 import { MindARThree } from 'mindar-image-three';
 
-const container = document.createElement("div")
-container.id = "container"
-document.body.appendChild(container)
-
-const mindARThree = new window.MINDAR.IMAGE.MindARThree({
-    container: container,
-    imageTargetSrc: "../assets/targets/targets.mind"
-});
-
-const { renderer, scene } = mindARThree;
-
-let camera
-
-//let camera, renderer, scene
-
 init()
-StartUpdate()
 
 async function init() 
 {
-    document.body.appendChild(renderer.domElement)
+    const mindARThree = new window.MINDAR.IMAGE.MindARThree({
+        container: document.body,
+        imageTargetSrc: "../assets/targets/targets.mind"
+    });
     
-    camera = new THREE.PerspectiveCamera
-    (
-        100, 
-        window.innerWidth / window.innerHeight, 
-        0.1, 
-        1000
-    )
-    
-    // Create a scene
-    /*
-    scene = new THREE.Scene()
-    scene.name = "myScene"
-    */
+    const { renderer, scene, camera} = mindARThree;
 
-    scene.add(camera)
-    
     await mindARThree.start()
-}
 
-function StartUpdate() 
-{
-    renderer.setAnimationLoop(OnUpdate)
-}
+    renderer.setAnimationLoop(onUpdate)
 
-function OnUpdate() 
-{
-    renderer.render(scene, camera)
+    function onUpdate() {
+        renderer.render(scene, camera)
+    }
 }
