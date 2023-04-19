bfccee66898ecb6c003821d93e2e9dfeeef11125
LBG-JuhoT <juho.tommolalaune@gmail.com>
2023-04-19 23:19:54 +0300

    .

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ async function init()
         }
     );
 
-    linkedIn.position.set(0.7, 0, 0);
+    linkedIn.position.set(0, -0.45, 0);
     profileImage.position.set(-0.3, 0.45, 0);
     cssVideoObject.position.set(-0.3, -0.45, -0.01);
     YTVideoRaycastTarget.position.set(0, 0, 0.01);
