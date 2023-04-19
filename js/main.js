ba72d1924bb33e6c35e0a554e24931482e2e9883
LBG-JuhoT <juho.tommolalaune@gmail.com>
2023-04-19 19:14:45 +0300

    .

--- js/main.js
+++ js/main.js
@@ -10,21 +10,49 @@ let cssRenderer, renderer, cssScene, scene, camera;
 const videoIFrame = document.getElementById("player");
 
 
-/*
-iframe.addEventListener("load", function () {
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
 
-    var videoMaterial = new THREE.MeshBasicMaterial({
-        map: new THREE.VideoTexture(videoWindow.getElementsByTagName("video")[0])
-    });
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    var videoGeometry = new THREE.PlaneGeometry(0.6, 0.3375);
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'hzLdZWIeq3c',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
 
-    YTVideoRaycastTarget = new THREE.Mesh(videoGeometry, videoMaterial);
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
 
-    anchor.group.add(YTVideoRaycastTarget);
-
-});
-*/
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
 
 console.log(videoIFrame);
 
